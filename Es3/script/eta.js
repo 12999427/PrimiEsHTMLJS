46729b56ce821a1b2601797f4578dec0d18662e4
PERUZZI GIOVANNI <12999427@itisrossi.edu.it>
2024-10-24 10:31:08 +0200

    miglirato

--- Es3/script/eta.js
+++ Es3/script/eta.js
@@ -1,3 +1,7 @@
-let età = parseInt(prompt("inserisci età"))
-let puovotare = età>=25
-document.getElementById("text").innerHTML = puovotare ? "Si, puoi votare" : "No, sei troppo giovane"
+let età = parseInt(prompt("inserisci età"));
+let puovotare = età>=25;
+if (isNaN(età) || età<0){
+    document.getElementById("text").innerHTML = "Non valido";
+} else {
+    document.getElementById("text").innerHTML = puovotare ? "Si, puoi votare" : "No, sei troppo giovane";
+}
